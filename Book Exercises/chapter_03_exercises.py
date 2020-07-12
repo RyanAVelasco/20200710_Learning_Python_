4ee49bcfd443e12a37d738b0cc67c476d97706c2
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2020-07-11 21:19:28 -0500

    minor edits: c2, c3
    Minor edits to seperation of questions and answers.

--- Book Exercises/chapter_03_exercises.py
+++ Book Exercises/chapter_03_exercises.py
@@ -1,11 +1,13 @@
-# NOTE: Question 2 is the only concept I'm unfamiliar with (try/except)
+print("NOTE: Question 2 is the only concept I'm unfamiliar with (try/except)\n")
 
 
-# Question 1: Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked
-# above 40 hours.
-# Enter Hours: 45
-# Enter Rate: 10
-# Pay: 475.0
+print("""\n\n=== Question 1 ===
+Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worke
+above 40 hours.
+Enter Hours: 45
+Enter Rate: 10
+Pay: 475.0""")
+print("\n== Answer ==")
 hours = 45
 rate = 10
 if hours > 40:
@@ -14,36 +16,60 @@ else:
     print(rate * hours)  #  <= 40 would return max: 400
 
 
-# Question 2: Rewrite your pay program using try and except so that your program handles non-numeric input
-# gracefully by printing a message and exiting the program. The following shows two executions of the program:
-# Enter Hours: 20
-# Enter Rate: nine
-# Error, please enter numeric input
-# Enter Hours: forty
-# Error, please enter numeric input
+print("""\n\n=== Question 2 ===
+Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by 
+printing a message and exiting the program. The following shows two executions of the program:
+
+Enter Hours: 20
+Enter Rate: nine
+Error, please enter numeric input
+Enter Hours: forty
+Error, please enter numeric input""")
+print("\n== Answer ==" + " Part A ==")
+hours = 45
+rate = "nine"
+try:
+    if hours > 40:
+        print(rate * 1.5 * hours)
+    else:
+        print(rate * hours)  #  <= 40 would return max: 400
+except:
+	print("Error, please enter numeric input")
+
+print("\n== Answer ==" + " Part B ==")
+hours = "forty"
+rate = 9
+try:
+    if hours > 40:
+        print(rate * 1.5 * hours)
+    else:
+        print(rate * hours)  #  <= 40 would return max: 400
+except:
+	print("Error, please enter numeric input")
 
 
-# Question 3: Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range,
-# print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
-# >= 0.9 A
-# >= 0.8 B
-# >= 0.7 C
-# >= 0.6 D
-# < 0.6 F
-#
-# Enter score: 0.95
-# A
-# Enter score: perfect
-# Bad score
-# Enter score: 10.0
-# Bad score
-# Enter score: 0.75
-# C
-# Enter score: 0.5
-# F
-# Run the program repeatedly as shown above to test the various different values for input.
+print("""\n\n=== Question 3 ===
+Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range print an error message.
+If the score is between 0.0 and 1.0, print a grade using the following table:
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
 
-print("This is the initial code answering the question within the parameters of the question")
+Enter score: 0.95
+A
+Enter score: perfect
+Bad score
+Enter score: 10.0
+Bad score
+Enter score: 0.75
+C
+Enter score: 0.5
+F
+Run the program repeatedly as shown above to test the various different values for input.""")
+
+print("\n=== Answer === This is the initial code answering the question within the parameters of the question ==")
 score = input("Enter Score: ")
 try:
     score = float(score)  # Is there a more efficient way to do this?
@@ -73,7 +99,7 @@ except:
 # I wanted to make the code automatically enter the required values for the exercise so I coded a loop below updating
 # a 'n' variable for each iteration
 
-print("\n== This is my automatic code solving the solution ==")
+print("\n=== Answer === This is my automatic code solving the solution ==")
 count = 0
 n = 0
 while count < 5:  # these are the values told to input to test the code
@@ -109,4 +135,3 @@ while count < 5:  # these are the values told to input to test the code
         print("Bad score")
 
     count += 1
-    
