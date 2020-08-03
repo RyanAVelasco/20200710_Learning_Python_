08a9436ba61a7d9a032db67c0149a2f5f3d19342
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2020-08-03 18:54:25 -0500

    Completed some exercises

---
+++ Book Exercises/chapter_09_exercises.py
@@ -0,0 +1,150 @@
+# letters = """
+# Plot
+# See also: List of Black Butler characters
+# In Victorian-era London lives a thirteen-year-old earl named Ciel Phantomhive, who acquired this position after the events of December 14, 1885 (Ciel's 10th birthday), when the Phantomhive manor was attacked by unidentified perpetrators and was set ablaze. Ciel, amidst the chaos, discovers his parents, Vincent and Rachel Phantomhive, to be dead along with the family dog, Sebastian. The same night he is kidnapped by the attackers and then sold into slavery, where he ends up in the hands of a sadistic, demon-worshipping cult. Ciel then endures endless physical, mental and sexual abuse at the hands of his captors. Ciel was also heat branded with a mark referred to as the "mark of the beast". One night, during a sacrificial ceremony to summon a demon, instead of forming a contract with the cult members, the demon states that he was summoned by Ciel, therefore he only agrees to form a contract with him, killing all the cultist members in the process. To show a contract was formed, the demon places a contract symbol referred to as the Faustian contract symbol on Ciel's right eye, giving it a purple hue, and his iris and pupil now showcase the symbol of the covenant. After the formation of the contract, the demon reveals he will consume Ciel's soul as payment for helping him achieve his goal; revenge on those who brought down the House of Phantomhive. Afterwards Ciel names the demon Sebastian Michaelis, after his deceased pet dog. The duo then return to society as Ciel takes over his now late father's previous position as the queen's watchdog, a very high-profile individual who is tasked with investigating cases that Queen Victoria herself deems especially important or threatening to England and the crown.
+#
+# Media
+# Manga
+# Main article: List of Black Butler chapters
+# Written and illustrated by Yana Toboso, the chapters of Black Butler have been serialized in the shōnen manga magazine Monthly GFantasy since its debut in the October 2006 issue, published on September 16, 2006.[4][5] The chapters are collected and published into individual tankōbon volumes by Square Enix. The first volume was released on February 27, 2007.[6] As of December 27, 2019, twenty-nine volumes have been released.[7]
+#
+# Yen Press licensed the series for an English language release and serialized the manga in Yen Plus' August 2009 issue for the magazine's first anniversary.[8] The publisher released the first volume in January 2010.[9] The second volume was released in May 2010.[10]
+#
+# French publisher Kana licensed the series in under the Dark Kana imprint as Black Butler. The publisher released the first volume in November 2009.[11] Carlsen Comics has licensed the series in Germany as Black Butler, the same title used for the French and English releases. Black Butler have been also published in Italy by Panini Comics.[12] In Poland, Waneko is publishing the manga as original Kuroshitsuji with subtitle Mroczny kamerdyner – as of February 2013, nine volumes have been released.[13] The series has been published in Finland under the original Japanese name Kuroshitsuji by Punainen jättiläinen since July 2012. In Spain, the series has been published in Norma Editorial since November 2011 under the title Black Butler.[14]
+#
+# Drama CD
+# On August 10, 2007, a drama CD was released by Frontier Works. It featured many of the characters appearing in volumes one and two.[15] A second drama CD was released on November 26, 2008 under the Aniplex label.[16]
+#
+# Anime
+# Main article: List of Black Butler episodes
+# In July 2008, it was announced that an anime adaption of Black Butler, directed by Toshiya Shinohara and produced by A-1 Pictures. It premiered in October 2008 and broadcast on MBS as well as the TBS.[17][18][19] On January 1, 2009, a limited edition DVD containing the first episode was released by Aniplex.[20] The next three episodes were released on another DVD on February 25, 2009. On an event on June 14, 2009, it was announced that the anime would be returning for a second series. Japanese voice actor Junichi Suwabe confirmed this news on his official blog later that day.[21][22] The second series, Black Butler II, premiered in July and follows a butler, Claude Faustus, and his master, Alois Trancy, as well as Sebastian and Ciel. Both new characters were designed by Toboso.[23] Aniplex later released the complete Blu-ray and DVD on May 7, 2014.[24]
+#
+# On March 29, 2010, North American anime distributor Funimation announced on their online FuniCon 3.0 panel that they had licensed Black Butler.[25] At Anime Expo 2010, Funimation also announced that they would stream the simulcast series, Black Butler II.[26] Funimation announced on their Facebook page that they had fully licensed the second season. Funimation released Black Butler Combo Pack Blu-ray/DVD first and second season on April 3, 2012. The series made its North American television debut on February 8, 2011 on the Funimation Channel.[27] Funimation lost the rights to the series' first season in 2017,[28] and the rights to the second season in 2018.[29] The first season is currently licensed by Aniplex of America and it was released on a Blu-ray Disc box set on January 29, 2019.[30] In November 2019, Funimation re-licensed the first two seasons for its streaming service.[31]
+#
+# On January 16, 2014, it was announced that a third Black Butler anime series had been green-lit.[32][33][34] Titled Black Butler: Book of Circus, the series was a close adaptation of the manga, unlike the previous seasons, adapting the Noah's Ark Circus arc.[35] The series was directed by Noriyuki Abe at A-1 Pictures, with Hiroyuki Yoshino in charge of scripts, along with Ichiro Okuchi and Yuka Miyata as scriptwriters. The main cast from the previous anime series returned, along with new cast members, and the series aired from July 10, 2014, to September 12, 2014.[36] In addition, a two-part original video animation (OVA) titled the Book of Murder, an adaptation of the Phantomhive Manor Murders arc, was screened in Japanese theatres on October 25, and November 15, 2014. The OVAs featured returning staff from the previous season.[37][38]
+#
+# On August 7, 2014, Funimation announced that they have licensed Black Butler: Book of Circus,[39] and have streamed it on their simulcast. Funimation released the series on Blu-ray and DVD on April 19, 2016.[40] Additionally, Funimation has licensed the two-part OVA Black Butler: Book of Murder and released it on May 17, 2016.[40]
+#
+# Musical
+# That Butler, Friendship (その執事、友好, Sono Shitsuji, Yūkō), a musical adaptation of the manga, had run at the Sunshine Theater in Ikebukuro between May 28, 2009 and June 7, 2009.[41] Yuya Matsushita portrayed Sebastian Michaelis, Shougo Sakamoto played Ciel Phantomhive and Uehara Takuya as Grell Sutcliff.[42]
+#
+# Musical Black Butler: The Most Beautiful Death in The World – A Thousand Souls and The Fallen Grim Reaper (ミュージカル｢黒執事｣〜ザ・モースト・ビューティフル・デス・イン・ザ・ワールド〜千の魂と堕ちた死神, Miūjikaru Kuroshitsuji – Za Mōsuto Byūtifuru Desu in za Wārudo – Sen no Tamashii to Ochita Shinigami),[43] the second musical adaptation of the manga, ran at Akasaka Act Theater (1300 seats) in Akasaka, Tokyo, Nagoya, and Osaka, between May 3 and May 23, 2010. Yuya Matsushita reprised his role as Sebastian Michaelis, Yukito Nishii played Ciel Phantomhive, and Uehara Takuya reprised his role as Grell Sutcliff. The other two main characters, Eric Slingby and Alan Humphries, were portrayed by Taisuke Saeki and Shinya Matsumoto, respectively. The musical was written by Mari Okada, directed by Sakurako Fukuyama, with music composed by Taku Iwasaki, and lyrics by Yukinojo Mori. A second run of The Most Beautiful Death in the World was announced in December 2012. It ran between May 17 and June 9, 2013 in the Akasaka ACT Theater (Akasaka, Tokyo) again, and the Umeda Arts Theater in Osaka. It was announced in February 2013 that Yuya Matsushita, Uehara Takuya, and Shuhei Izumi would reprise their roles as Sebastian Michaelis, Grell Sutcliff, and the Undertaker, respectively. The rest of the roles were re-cast, with Taketo Tanaka replacing Yukito Nishii as Ciel Phantomhive, and Shinji Rachi and Masataka Nakagauchi replacing Taisuke Saeki and Matsumoto Shinya in the roles of Eric Slingby and Alan Humphries.[44]
+#
+# A third musical Lycoris that Blazes the Earth (地に燃えるリコリス, Chi ni Moeru Lycoris) was performed in September 2014. Most of the cast reprised their roles from the re-run of The Most Beautiful Death in the World although Fukuzaki Nayuta replaced Taketo Tanaka as Ciel Phantomhive and Yuka Terasaki replaced Saki Matsuda as Mey-Rin. Akane Liv was introduced as Madam Red, Yuusuke Hirose as Charles Phipps, Oota Motohiro as Charles Grey and Araki Hirofumi as Lau.[45] A re-run of the third Black Butler musical "Lycoris that Blazes the Earth" was scheduled to perform in November–December 2015.[46] It started in Osaka on November 7 then travel to Miyagi, Tokyo and Fukuoka.[47] The new run of the third musical was the first oversea tour in China (Shanghai, Beijing, Shenzhen) in December 2015.[47] Yuta Furukawa is replacing Yuya Matsushita as Sebastian Michaelis, who had been playing Sebastian since the first musical.[46] Meanwhile, most of the cast from the third musical reprise their roles.
+#
+# A fourth Black Butler musical, based on the Noah's Ark Circus arc of the manga, ran between November–December 2016. Yuta Furukawa reprised his role as Sebastian Michaelis, with Reo Uchikawa replacing Nayuta Fukuzaki as Ciel. Miura Ryosuke was introduced as Joker, Tano Asami as Beast and Tamaki Yuki as Snake, among others. Notably, Izumi Shuuhei reprised his role as the Undertaker again, making him the only original cast member still performing.[48]
+#
+# A fifth Black Butler musical, Tango on the Campania, which was based on the Luxury Liner arc in the manga, ran between December 2017 to February 2018. Yuta Furukawa reprised his role as Sebastian Michaelis, with Reo Uchikawa also reprising his role as Ciel Phantomhive. Mikata Ryosuke was introduced as Ronald Knox, Okazaki Momoko as Elizabeth Midford, and others. Other actors from previous performers also reprised their roles, such as Uehara Takuya as Grell Sutcliff, Izumi Shuuhei as the Undertaker, and Sasaki Yoshihide as Viscount Druitt.[49][50]
+#
+# Video game
+# A video game for Nintendo DS, called Kuroshitsuji Phantom & Ghost was developed by Square Enix and was released on March 19, 2009.[51] The game is sold in two versions, a limited first-press edition with a higher price and numerous extra goods, and a regular edition.
+#
+# Print
+# On February 27, 2009, the Black Butler Character Book "That Butler, Assembles" (黒執事 キャラクターガイド ｢その執事、集合｣, Kuroshitsuji Character Guide "Sono Shitsuji, Shūgō") was released.[52] The TV Animation Black Butler Black Record (TVアニメーション｢黒執事｣ Black Record) was released on March 27, 2009.[53] An official comics anthology Rainbow Butler (虹執事, Nijishitsuji) was also released on that date.[54]
+#
+# Films
+# Main articles: Black Butler (film) and Black Butler: Book of the Atlantic
+# It was announced on January 18, 2013 that the manga would be made into a live-action film starring Ayame Goriki as Genpou Shiori, Hiro Mizushima as Sebastian Michaelis. Filming began in April 2013 and the film was released on January 18, 2014.[55][56]
+#
+# On October 10, 2015, it was announced an animated film for the series has been green-lit, and the original anime cast returned to reprise their roles.[57] On February 17, 2016, the film Black Butler: Book of the Atlantic, was confirmed to be an animated adaptation of the Luxury Liner (豪華客船, Gōka Kyakusen) arc from the original manga. It was released in Japan on January 21, 2017.[57][58] On April 1, 2017, Funimation announced that they have licensed the animated film and would screen it in a limited theatrical release in North America later in the year.[59] Black Butler: Book of the Atlantic, premiered in Japan on January 21, 2017, and in North America on the selected dates of June 12 and 14, 2017.
+#
+# Reception
+# As of July 2019, Black Butler has sold over 28 million copies worldwide.[60] Individual volumes alone have done well in weekly polls in Japan, taking several high spots.[61][62][63][64] Black Butler has a large fan base that is very supportive. In addition to the volumes, the comics anthology Rainbow Butler ranked seventeenth during the week of March 31 to April 6 selling 41,083 copies after ranking thirty-third the previous week.[65] The fourth and fifth volumes ranked thirty-third and thirty-ninth of the top-selling manga in Japan during 2008, selling 529,210 copies and 468,550 copies respectively.[66] The sixth volume ranked thirteenth of the top-selling manga in Japan during the first half of 2009, selling 619,501 copies.[67] The series itself was ranked tenth of the top-selling manga series in Japan during 2009, selling a total of 1,603,197 estimated copies. The series is also quite popular in other countries.[68]
+#
+# The Japanese music distributor Recochoku has created an annual survey of which anime characters that people would like to marry. Sebastian Michaelis ranked ninth in the category "The Character I Want to Be My Groom".[69]
+#
+# By 2 February, the film had grossed ¥479,554,245 (US$4,687,163) at the Japanese box office.[70]
+# """
+#
+# letters = letters.split()
+# words = dict()
+# for x in letters:
+#     if x not in words:
+#         words[x] = 1
+#     else:
+#         words[x] += 1
+#
+# ol = list(words.keys())
+# ol.sort()
+#
+# for keys in ol: print(keys, "appears", words[keys], "times")
+
+import string
+
+print("""
+
+
+Exercise 1: Download a copy of the file www.py4e.com/code3/words.txt
+
+Write a program theat reads the words in words.txt and stores them as keys in a dictionary. It
+doesn't matter what the values are. Then you can use the in operator as a fast way to check
+whether a string is in the dictionary.
+""")
+
+print("""
+=== Answer ===
+'This effectively a word counter'
+""")
+# fhand = open("words.txt")
+# words = dict()  # alternative: words = {}
+#
+# for lines in fhand:
+#     lines = lines.rstrip()
+#     line = lines.split()
+#     for word in line:
+#         if word not in words:
+#             words[word] = 1
+#         else:
+#             words[word] += 1
+
+# print(words)
+# print("data" in words)  # replace string to see if word in dict
+
+#  Simplfied version
+fhand = open("words.txt")
+words = dict()  # alternative: words = {}
+
+for lines in fhand:
+    lines = lines.rstrip()
+    lines = lines.translate(lines.maketrans("", "", string.punctuation))  # added this in from 9.4 Advance text parsing
+    lines.lower()  # same as above
+    line = lines.split()
+    for word in line:
+        words[word] = words.get(word, 0) + 1  # Not sure what the 0 refers to in the 2nd parameter
+
+# for key in words:
+    # print("Word:", key, "| Number:", words[key])
+
+lst = list(words.keys())
+lst.sort()
+for key in lst:
+    print(key, words[key])
+
+print("""
+
+Exercise 2: Write a program that categorizes each mail message by which day of the week
+the commit was done. To do this look for lines that start with 'From', then look for the
+third word and keep a running count of each of the days of the week. At the end of the 
+program print out the contents of your dictionary (order does not matter).
+
+Sample Line:
+
+From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+
+Sample Execution:
+
+python dow.py
+Enter a file name: mbox-short.txt
+{'Fri': 20, 'Thu': 6, 'Sat': 1}
+""")
+
+print("""
+=== Answer ===
+""")
+
+fname = open("mbox-short.txt")
+date = dict()
+for lines in fname:
+    
+    if lines.startswith("From")
+
