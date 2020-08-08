1252a2d5517f9f7d23aa6179b98746c4dd3644b2
RyanAVelasco <45979367+RyanAVelasco@users.noreply.github.com>
2020-08-07 23:44:43 -0500

    Completed chapters 9, 10

--- Book Exercises/chapter_09_exercises.py
+++ Book Exercises/chapter_09_exercises.py
@@ -145,6 +145,101 @@ print("""
 fname = open("mbox-short.txt")
 date = dict()
 for lines in fname:
-    
-    if lines.startswith("From")
+    if lines.startswith("From"):
+        stuff = lines.split()
+        for day in stuff:
+            if "Mon" == day or \
+                "Tue" == day or \
+                "Wed" == day or \
+                "Thu" == day or \
+                "Fri" == day or \
+                "Sat" == day or \
+                "Sun" == day:
+                date[day] = date.get(day, 0) + 1
 
+print(date)
+
+print("""
+
+Exercise 3: Write a program to read through a mail log, build a histogram using a dictionary
+to count how many messages have come from each email address, and print the dictionary.""")
+
+fname = open("mbox-short.txt")
+email = dict()
+for lines in fname:
+    if lines.startswith("From"):
+        stuff = lines.split()
+        for day in stuff:
+            if "@" in day:
+                email[day] = email.get(day, 0) + 1
+
+print(email)
+
+print("""
+
+
+Exercise 4: Add code to the above program to figure out who has the
+most messages in the file. After all the data has been read and the 
+dictionary has been created, look through the dictionary using a maximum
+loop (see Chapter 5: Maximum and minimum loops) to find who has
+the most messages and print how many messages the person has.
+
+Enter a file name: mbox-short.txt
+cwen@iupui.edu 5
+
+Enter a file name: mbox.txt
+zqian@umich.edu 195
+""")
+
+print("""
+=== Answer ===""")
+
+fname = open("mbox.txt")
+email = dict()
+largest = None
+for lines in fname:
+    if lines.startswith("From"):
+        stuff = lines.split()
+        for day in stuff:
+            if "@" in day:
+                email[day] = email.get(day, 0) + 1
+
+for itervar in email:
+    if largest is None or itervar > largest:
+        largest = itervar
+    # print("Loop:", itervar, largest, email[largest])
+print("Most messages from:", largest, "at", email[largest], "emails")
+
+
+print("""
+
+
+Exercise 5: This program records the domain name (instead of the
+address) where the message was sent from instead of who the mail came
+from (i.e., the whole email address). At the end of the program, print
+out the contents of your dictionary.
+python schoolcount.py
+
+Enter a file name: mbox-short.txt
+
+{'media.berkeley.edu': 4, 'uct.ac.za': 6, 'umich.edu': 7,
+'gmail.com': 1, 'caret.cam.ac.uk': 1, 'iupui.edu': 8}
+""")
+
+print("""
+=== Answer ===
+""")
+
+domain = dict()
+
+fname = open("mbox-short.txt")
+for lines in fname:
+    if lines.startswith("From"):
+        lines = lines.split()
+        lines = lines[1].split("@")
+        dom = lines[1]
+        if dom not in domain:
+            domain[dom] = 1
+        else:
+            domain[dom] += 1
+print(domain)
